7efeb075dd4a5c50a3e9184c99129551a1b080d8
zainsubhani <96117969+zainsubhani@users.noreply.github.com>
2025-04-23 16:20:31 +0200

    add Trash icon add NoteCard NotesPages

--- src/pages/NoteCard.jsx
+++ src/pages/NoteCard.jsx
@@ -1,6 +1,25 @@
-const NoteCard = ({ note }) => {
+export const NoteCard = ({ note }) => {
+  let position = JSON.parse(note.position);
+  const colors = JSON.parse(note.colors);
   const body = JSON.parse(note.body);
 
-  return <div>{body}</div>;
+  return (
+    <div
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+      }}
+    >
+      <div
+        className="card-header"
+        style={{ backgroundColor: colors.colorHeader }}
+      ></div>
+      <div className="card-body">
+        <textarea
+          style={{ color: colors.colorText }}
+          defaultValue={body}
+        ></textarea>
+      </div>
+    </div>
+  );
 };
-export default NoteCard;
--- src/pages/NotesPages.jsx
+++ src/pages/NotesPages.jsx
@@ -1,4 +1,12 @@
-const NotesPage = () => {
-  return <div>NotesPage</div>;
+import { fakeData as notes } from "../assets/fakeData.js";
+import NoteCard from "../components/NoteCard";
+
+export const NotesPage = () => {
+  return (
+    <div>
+      {notes.map((note) => (
+        <NoteCard note={note} key={note.$id} />
+      ))}
+    </div>
+  );
 };
-export default NotesPage;
